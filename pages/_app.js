ada4d8018f2dfa00877c12457a5bbd6f0bdee1ed
yomarguti <yomar.guti@gmail.com>
2020-11-10 15:41:34 -0500

    Base configuration of the project: Nextjs, React, Tailwind

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/tailwind.css';
+
+function App({ Component, pageProps }) {
+  //Providers -Context Theme - Data
+  //Layout
+  // props adicionales
+  return <Component {...pageProps} />;
+}
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const index = () => {
+  return <div>Pagina de inicio de mi portafolio</div>;
+};
+
+export default index;
