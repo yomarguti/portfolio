e1ba3ed3ff5891a4efad81b7c4c93861a85d47f7
yomarguti <yomar.guti@gmail.com>
2020-11-13 06:15:03 -0500

    Config favicon.ico setup on _document.js with realfavicongenerator.net

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import '../styles/tailwind.css';
+import Head from 'next/head';
 
 function App({ Component, pageProps }) {
   //Providers -Context Theme - Data
   //Layout
   // props adicionales
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Yomar Gutierrez | Fullstack Web Developer</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default App;
