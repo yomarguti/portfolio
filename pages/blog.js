4ff9ca6cfb2f04b335481762fffab16d7c2cadd4
yomarguti <yomar.guti@gmail.com>
2021-03-28 07:14:37 -0500

    Agregado header a la pagina /blog

---
+++ pages/blog.js
@@ -0,0 +1,12 @@
+const Blog = () => {
+  return (
+    <>
+      <header className="w-full px-4 bg-blue-300 md:py-10">
+        <h1>Blogs</h1>
+        <p>Adentrarse en un nuevo mundo de conocimientos </p>
+      </header>
+    </>
+  );
+};
+
+export default Blog;
