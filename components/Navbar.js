cc63fa396bf071c45932900e2876737ed7bc2b1a
yomarguti <yomar.guti@gmail.com>
2021-03-27 20:43:44 -0500

    Menu en ingles. Agregado el menu Blog. Modificados los href de cada menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,18 +3,18 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <ul className="w-full text-sm md:text-base flex justify-center items-center h-16 md:border md:border-gray-400 md:shadow rounded-sm font-bold text-white bg-green-900">
+    <ul className="flex items-center justify-center w-full h-16 text-sm font-bold text-white bg-green-900 rounded-sm md:text-base md:border md:border-gray-400 md:shadow">
       <Link href="/">
-        <li className="p-4 cursor-pointer hover:bg-green-800">Inicio</li>
-      </Link>
-      <Link href="/study">
-        <li className="p-4 cursor-pointer hover:bg-green-800">Estudios</li>
+        <li className="p-4 cursor-pointer hover:bg-green-800">Home</li>
       </Link>
       <Link href="/skills">
-        <li className="p-4 cursor-pointer hover:bg-green-800">Habilidades</li>
+        <li className="p-4 cursor-pointer hover:bg-green-800">Skills</li>
       </Link>
-      <Link href="/about">
-        <li className="p-4 cursor-pointer hover:bg-green-800">Sobre mí</li>
+      <Link href="/blog">
+        <li className="p-4 cursor-pointer hover:bg-green-800">Blog</li>
+      </Link>
+      <Link href="/me">
+        <li className="p-4 cursor-pointer hover:bg-green-800">About Me</li>
       </Link>
     </ul>
   );
