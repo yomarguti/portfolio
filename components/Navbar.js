01a3459bdbd65a2e5a3b44dbc3e2c929b2613ef4
yomarguti <yomar.guti@gmail.com>
2021-03-29 17:00:40 -0500

    Cambio de menu /skills por /education, Tamaño de letra a text-base

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,12 +2,12 @@ import NavItem from './NavItem';
 
 const Navbar = () => {
   return (
-    <nav className="w-full h-16 text-sm text-gray-800 md:text-base">
-      <ul className="flex items-center justify-center h-full tracking-wider sm:shadow">
+    <nav className="w-full h-16 text-base text-gray-800 md:text-base">
+      <ul className="flex items-center justify-center h-full sm:tracking-wider sm:shadow">
         <NavItem path="/" menuName="Home" />
-        <NavItem path="/skills" menuName="Skills" />
+        <NavItem path="/education" menuName="Education" />
         <NavItem path="/blog" menuName="Blog" />
-        <NavItem path="/me" menuName="About Me" />
+        <NavItem path="/me" menuName="About" />
       </ul>
     </nav>
   );
