496a0fed2def41d318d444295f757ae8bcb44a6e
yomarguti <yomar.guti@gmail.com>
2021-03-29 17:16:02 -0500

    Cambio en line-height del parrafo principal y la fecha de los posts, en la lista

--- components/BlogListItem.js
+++ components/BlogListItem.js
@@ -4,7 +4,7 @@ const BlogListItem = ({ post: { id, title, summary, date } }) => {
   return (
     <article className="flex flex-col items-start py-4 sm:px-3 sm:shadow-md">
       <h1 className="text-lg font-semibold">{title}</h1>
-      <p className="text-xs italic text-gray-400">{date}</p>
+      <p className="text-xs italic text-gray-700">{date}</p>
       <p className="pt-2 tracking-tight">{summary}</p>
       <Link href={`/blog/${id}`}>
         <span className="self-end px-3 py-1 mt-2 text-white border rounded-lg cursor-pointer bg-primary">
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -16,7 +16,7 @@ const Blog = ({ postsData }) => {
     <main className="w-full mx-auto text-gray-800 md:w-content-md lg:w-content-lg">
       <section className="px-4 pt-5 pb-5 border-b-2 md:pt-10">
         <h1 className="text-4xl font-extrabold">Blog</h1>
-        <p className="pt-4 text-lg leading-snug tracking-wide sm:pt-6">
+        <p className="pt-4 text-lg leading-tight sm:leading-snug sm:tracking-wide sm:pt-6">
           Adentrarse en un mundo nuevo de conocimientos es un reto grandísimo para cualquier
           persona. Es una lucha constante en contra de tu propio cuerpo que intenta seguir en la
           rutina, seguir en su estado de tranquilidad y confort. Por esto, es necesario armarse de
